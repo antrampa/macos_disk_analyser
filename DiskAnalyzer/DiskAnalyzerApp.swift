ee937328730f6e46bf4a6eb2904dae0d92789866
Antonis Zalonis <antonis.zalonis@gmail.com>
2025-04-30 00:27:02 +0200

    Initial Commit

---
+++ DiskAnalyzer/DiskAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskAnalyzerApp.swift
+//  DiskAnalyzer
+//
+//  Created by Antonis on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiskAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
